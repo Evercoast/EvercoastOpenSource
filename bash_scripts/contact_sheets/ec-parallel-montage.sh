ea4cd23fdbb4421a16c72cf9cdf8299c0ae1fca8
Sebastian Marino <smm@evercoast.com>
2020-09-02 18:09:05 -0400

    Updated internal docs.

--- bash_scripts/contact_sheets/ec-parallel-montage.sh
+++ bash_scripts/contact_sheets/ec-parallel-montage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # example usage:
-# ec-parallel_montage.sh 194 1 406 ec-montage.sh 1x1 50
+# ec-parallel-montage.sh 1 2 100 ec-montage.sh 10x2 50
 
-# 194 = start frame
-# 1 = frame increment
-# 486 = end frame
+# 1             = start frame
+# 2             = frame increment
+# 100           = end frame
 # ec-montage.sh = montage script to be executed
-# 1x1 = montage tiling
-# 50 = image scaling
+# 1x1           = montage mxn tiling
+# 50            = result image scaling
 
 for i in $(seq $1 $2 $3); do
 	echo $4 $5 $6 $i contact.`printf %07d $i`.png >> .ec_montage
