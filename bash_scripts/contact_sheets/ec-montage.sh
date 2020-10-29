bde990e5d8928c2cc5a3f83c18afc1df78627c6c
Sebastian Marino <smm@evercoast.com>
2020-10-28 20:14:44 -0400

    More long sequence hacks.

--- bash_scripts/contact_sheets/ec-montage.sh
+++ bash_scripts/contact_sheets/ec-montage.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-montage -rotate 90 -tile $1 -geometry +0+0 -gravity center -resize $2% -background grey42 -label %t *00$3.png $4
+montage -rotate 90 -tile $1 -geometry +0+0 -gravity center -resize $2% -background grey42 -label %t *000$3.png $4
